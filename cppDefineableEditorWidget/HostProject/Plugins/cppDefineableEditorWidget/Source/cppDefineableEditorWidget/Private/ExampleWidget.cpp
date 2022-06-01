638fc95622da1246fc4bd42ddba4cf9145f8c3ce
Paxton89 <57005070+Paxton89@users.noreply.github.com>
2022-06-01 16:29:08 +0200

    1st

---
+++ cppDefineableEditorWidget/HostProject/Plugins/cppDefineableEditorWidget/Source/cppDefineableEditorWidget/Private/ExampleWidget.cpp
@@ -0,0 +1,72 @@
+﻿#include "ExampleWidget.h"
+#include "Components/Button.h"
+#include "Components/HorizontalBox.h"
+#include "Components/TextBlock.h"
+#include "Components/EditableTextBox.h"
+#include "Components/VerticalBox.h"
+#include "EngineUtils.h"
+
+UExampleWidget::UExampleWidget()
+{
+}
+
+void UExampleWidget::UpdateText()
+{
+	ToolNameTextBlock->SetText(FText::FromString(ToolNameText));
+}
+
+void UExampleWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	UpdateText();
+}
+
+void UExampleWidget::DiscoButtonPressed()
+{
+	DiscoButton->SetBackgroundColor(FLinearColor::MakeRandomColor());
+	auto Text = DiscoButton->GetChildAt(0);
+	Cast<UTextBlock>(Text)->SetColorAndOpacity(FLinearColor::MakeRandomColor());
+	
+}
+
+void UExampleWidget::UsefulButtonPressed()
+{
+	TActorIterator<AActor> Actoritr = TActorIterator<AActor>(GetWorld());
+	while (Actoritr)
+	{
+		FRotator rotation = FRotator(0,15,0);
+		Actoritr->SetActorRotation(Actoritr->GetActorRotation() + rotation);
+		++Actoritr;
+	}
+}
+
+void UExampleWidget::RenameButtonPressed()
+{
+	if(myTextBox->Text.IsEmpty())
+		return;
+	
+	int counter = 1;
+	TActorIterator<AActor> Actoritr = TActorIterator<AActor>(GetWorld());
+	while (Actoritr)
+	{
+		//	Increase counter if new name already exist - then skip renaming this actor
+		if(!Actoritr->IsSelected())
+		{
+			auto BaseNameAlreadyExist = Actoritr->GetActorLabel().Contains(myTextBox->Text.ToString());
+			auto NameHasThisNumberAlready = Actoritr->GetActorLabel().Contains(FString::FromInt(counter));
+			if(BaseNameAlreadyExist && NameHasThisNumberAlready)
+			{
+				counter++;
+			}
+		}
+		//	Rename this actor
+		else
+		{
+			FString UserText = myTextBox->Text.ToString();
+			UserText.AppendInt(counter);
+			Actoritr->SetActorLabel(UserText);
+			counter++;
+		}
+		++Actoritr;	
+	}
+}
---
+++ cppDefineableEditorWidget/HostProject/Plugins/cppDefineableEditorWidget/Source/cppDefineableEditorWidget/Public/ExampleWidget.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "Editor/Blutility/Classes/EditorUtilityWidget.h"
+#include "ExampleWidget.generated.h"
+
+class UHorizontalBox;
+class UVerticalBox;
+class UTextBlock;
+class UButton;
+class UEditableTextBox;
+
+UCLASS(BlueprintType)
+class UExampleWidget : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+public:
+	UExampleWidget();
+	virtual void NativeConstruct() override;
+	void UpdateText();
+	
+		UFUNCTION(BlueprintCallable)
+	void DiscoButtonPressed();
+		UFUNCTION(BlueprintCallable)
+	void UsefulButtonPressed();
+		UFUNCTION(BlueprintCallable)
+	void RenameButtonPressed();
+
+	//--	ToolTitle	--//
+		UPROPERTY(meta=(BindWidget), BlueprintReadWrite)
+	UVerticalBox* MAIN_VertBox;
+		UPROPERTY(meta=(BindWidget), BlueprintReadWrite)
+	UHorizontalBox* TitleHzBox;
+		UPROPERTY(meta=(BindWidget), BlueprintReadWrite)
+	UTextBlock* ToolNameTextBlock;
+	FString ToolNameText = TEXT("ToolName");
+
+#pragma region VertBox_Buttons
+		UPROPERTY(meta=(BindWidgetOptional), BlueprintReadWrite)
+	UButton* DiscoButton;
+		UPROPERTY(meta=(BindWidgetOptional), BlueprintReadWrite)
+	UButton* UsefulButton;
+		UPROPERTY(meta=(BindWidgetOptional), BlueprintReadWrite)
+	UButton* RenameButton;
+#pragma endregion VertBox_Buttons
+#pragma region VertBox_Description
+
+#pragma endregion VertBox_Description
+#pragma region VertBox_3
+		UPROPERTY(meta=(BindWidget))
+	UEditableTextBox* myTextBox;
+#pragma endregion VertBox_3
+};
